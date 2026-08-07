aeaeb0d0edf5a82990d14f83b8db84bc0a728025
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2026-08-07 09:44:34 +0200

    ⬆️ Bruk BOM for å sette micrometer på versjon 1.16.6

--- src/main/kotlin/no.nav.helse.sas.sas-kotlin.gradle.kts
+++ src/main/kotlin/no.nav.helse.sas.sas-kotlin.gradle.kts
@@ -18,6 +18,7 @@ kotlin {
 
 dependencies {
     implementation(platform("io.ktor:ktor-bom:3.5.1"))
+    implementation(platform("io.micrometer:micrometer-bom:1.16.6"))
     implementation(platform("io.netty:netty-bom:4.2.16.Final"))
     implementation(platform("io.prometheus:prometheus-metrics-bom:1.8.0"))
     implementation(platform("tools.jackson:jackson-bom:3.2.1"))
